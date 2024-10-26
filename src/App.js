ed2bc83d82f00d3239b442e83f569df2c33b79f1
FyteBit <Fytebitsoftware@gmail.com>
2024-10-26 15:48:31 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const tg = window.Telegram?.WebApp
+  console.log(tg)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex items-center justify-center">
+      <p>{tg.initData?`${tg.initDataUnsafe}`:"not tg"}</p>
     </div>
   );
 }
